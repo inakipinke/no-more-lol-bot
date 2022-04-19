7ffe92c2d202438c6d566dc5e6b1690155f7d9e7
Misael <misael705@gmail.com>
2022-04-19 12:41:16 -0300

    first commit, server express and base server

---
+++ index.js
@@ -0,0 +1,59 @@
+require('dotenv').config(); //initialize dotenv
+const { Client, Intents } = require('discord.js');
+var moment = require('moment')
+
+const express = require('express')
+const app = express()
+
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
+var mainChannel
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+  mainChannel = client.channels.cache.get("965795997127180329")
+
+});
+client.on('messageCreate', (message) => {
+  console.log("We got message, it says: ", message.content)
+  if (message.content == "semen") {
+    message.reply("Gordo teton deja del lol de una vez por todas")
+    console.log(message, "message")
+  }
+});
+
+client.on('presenceUpdate', (oldMember, newMember) => {
+  // do your thing
+
+  if (newMember.activities.length === 0) return
+  console.log(newMember, "member")
+  for (let index = 0; index < newMember.activities.length; index++) {
+    const element = newMember.activities[index];
+    console.log(element)
+    if (element.applicationId === '401518684763586560') {
+      mainChannel.send(`@everyone , ALGUIEN ESTA JUGANDO AL LOL`)
+    }
+    // } else {
+    //   setTimeout(getTimestimestamps, 5000, element);
+
+    // }
+  }
+});
+// function getTimestimestamps(element) {
+
+//   let propuesta = moment().diff(moment.utc(element.timestamps.start), "seconds")
+//   console.log(propuesta)
+//   if (propuesta >= 2) {
+
+//   }
+// }
+
+// Login to Discord with your client's token
+client.login(process.env.CLIENT_TOKEN); //login bot using token
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
